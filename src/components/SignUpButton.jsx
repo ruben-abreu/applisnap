197a248d2dd787e82dc8af396e9f30e7e427d109
anapatriciagomes <anapatrigomes@gmail.com>
2024-02-24 15:13:16 +0000

    sign up form style updated

--- src/components/SignUpButton.jsx
+++ src/components/SignUpButton.jsx
@@ -75,15 +75,7 @@ function SignUpButton() {
     '.MuiInputLabel-shrink': {
       color: '#678B85',
     },
-    '.MuiInputLabel-shrink:before': {
-      color: '#678B85',
-    },
-    '.MuiInputLabel-shrink:after': {
-      color: '#678B85',
-    },
-    '.MuiInputLabel-shrink:active': {
-      color: '#678B85',
-    },
+
     '.MuiInput-underline:after': {
       borderBottom: `2px solid #678B85`,
     },
