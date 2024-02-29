077555fd3e36916e5586340c2637986f945bfdc3
anapatriciagomes <anapatrigomes@gmail.com>
2024-02-29 16:16:45 +0000

    homepage update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import { useContext } from 'react';
 import SignUpButton from '../components/SignUpButton';
 import { ThemeContext } from '../context/theme.context';
+import { AuthContext } from '../context/auth.context';
 
 function HomePage() {
   const { darkMode } = useContext(ThemeContext);
+  const { loggedIn } = useContext(AuthContext);
 
   return (
-    <div className='h-full min-h-[80vh] mx-[2%] flex flex-col justify-center items-center'>
+    <div className="h-full min-h-[80vh] mx-[2%] flex flex-col justify-center items-center">
       <h1
         className={`${
           darkMode ? 'white' : 'text-[#677f8b]'
@@ -14,8 +16,8 @@ function HomePage() {
       >
         AppliSnap
       </h1>
-      <h2 className='mb-[30px]'>Job Application Tracker</h2>
-      <SignUpButton />
+      <h2 className="mb-[30px]">Job Application Tracker</h2>
+      {!loggedIn && <SignUpButton />}
     </div>
   );
 }
