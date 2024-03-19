a54ccefc161938c1b4f4eba5d4ba6b09f1fc25ec
anapatriciagomes <anapatrigomes@gmail.com>
2024-03-19 12:28:09 +0000

    edit application timeline correction

--- src/components/EditApplication.jsx
+++ src/components/EditApplication.jsx
@@ -661,7 +661,7 @@ function EditApplication({
             {editDate.rejected && (
               <TimelineItem>
                 <TimelineOppositeContent color="text.secondary">
-                  Offer
+                  Rejected
                 </TimelineOppositeContent>
                 <TimelineSeparator>
                   <TimelineDot />
