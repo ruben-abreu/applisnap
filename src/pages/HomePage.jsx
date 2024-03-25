92899c083cd36c309ea4a53bac99ab92da8eacea
anapatriciagomes <anapatrigomes@gmail.com>
2024-03-25 13:53:30 +0000

    homepage update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,12 @@ import boardPreview from '../assets/boardPreview.png';
 import boardPreviewDarkMode from '../assets/boardPreviewDarkMode.png';
 import boardPreviewMobile from '../assets/boardPreviewMobile.png';
 import boardPreviewDarkModeMobile from '../assets/boardPreviewDarkModeMobile.png';
+import insightsPreview from '../assets/insightsPreview.png';
+import insightsPreviewDarkMode from '../assets/insightsPreviewDarkMode.png';
+import insightsPreviewMobile from '../assets/insightsPreviewMobile.png';
+import insightsPreviewDarkModeMobile from '../assets/insightsPreviewDarkModeMobile.png';
 import LogInButton from '../components/LogInButton';
+import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
 
 function HomePage({ setCreditsPage }) {
   const { darkMode, width } = useContext(ThemeContext);
@@ -32,19 +37,63 @@ function HomePage({ setCreditsPage }) {
           </div>
         )}
       </div>
-      <img
-        src={
-          width < 500 && darkMode
-            ? boardPreviewDarkModeMobile
-            : width < 500
-            ? boardPreviewMobile
-            : darkMode
-            ? boardPreviewDarkMode
-            : boardPreview
-        }
-        alt="board preview"
-        className="w-[70%] border-solid border-8 border-[#ff8956] rounded-md"
-      />
+
+      <div className="flex max-[500px]:flex-col justify-center items-center gap-[30px] max-[685px]:gap-[15px] max-[500px]:gap-[30px]">
+        <ul className="flex flex-col gap-[20px] max-[685px]:gap-[10px]">
+          <li>
+            <b className="text-[#ff8956] text-lg">Job Hunt Made Easy</b>
+            <br />
+            <CheckRoundedIcon className="mr-[5px] text-[#ff8956]" /> Your
+            Personal Application Command Center!
+          </li>
+          <li>
+            <b className="text-[#ff8956] text-lg">Take Control</b>
+            <br />
+            <CheckRoundedIcon className="mr-[5px] text-[#ff8956]" /> Organize,
+            Track, and Conquer Your Job Search!
+          </li>
+          <li>
+            <b className="text-[#ff8956] text-lg">Never Miss a Beat</b>
+            <br />
+            <CheckRoundedIcon className="mr-[5px] text-[#ff8956]" /> Instant
+            Insights into Your Job Applications!
+          </li>
+          <li>
+            <b className="text-[#ff8956] text-lg">Job Search 2.0</b>
+            <br />
+            <CheckRoundedIcon className="mr-[5px] text-[#ff8956]" /> Power Up
+            Your Hunt with Seamless Tracking!
+          </li>
+        </ul>
+        <div className="w-[40%] max-w-[550px] max-[685px]:w-[60%] max-[500px]:w-[70%] p-[20px] flex flex-col gap-[10px] border-solid border-8 border-[#ff8956] rounded-md">
+          <img
+            src={
+              width < 500 && darkMode
+                ? boardPreviewDarkModeMobile
+                : width < 500
+                ? boardPreviewMobile
+                : darkMode
+                ? boardPreviewDarkMode
+                : boardPreview
+            }
+            alt="board preview"
+            className=""
+          />
+          <img
+            src={
+              width < 500 && darkMode
+                ? insightsPreviewDarkModeMobile
+                : width < 500
+                ? insightsPreviewMobile
+                : darkMode
+                ? insightsPreviewDarkMode
+                : insightsPreview
+            }
+            alt="board preview"
+            className=""
+          />
+        </div>
+      </div>
     </div>
   );
 }
