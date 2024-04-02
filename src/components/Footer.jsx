4d94ffcc52ab88a2bfc6314c2e0741556d514bd4
anapatriciagomes <anapatrigomes@gmail.com>
2024-04-02 15:46:55 +0100

    adding privacy policy to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,8 +11,12 @@ function Footer() {
     navigate('/credits');
   };
 
+  const handlePrivacyPolicyClick = () => {
+    navigate('/privacy-policy');
+  };
+
   return (
-    <div className="w-full bottom-0 left-0 text-center">
+    <div className="w-full bottom-0 left-0 text-center flex justify-center gap-[20px]">
       <button
         onClick={handleCreditsClick}
         className={`mx-auto px-[15px] py-[10px] my-[30px] rounded-md ${
@@ -24,6 +28,18 @@ function Footer() {
         } max-[450px]:text-[0.8em]`}
       >
         Credits
+      </button>
+      <button
+        onClick={handlePrivacyPolicyClick}
+        className={`mx-auto px-[15px] py-[10px] my-[30px] rounded-md ${
+          darkMode ? 'hover:text-black' : 'hover:text-white'
+        }  ${darkMode ? 'hover:bg-[#ff8956]' : 'hover:bg-[#f06c35]'} ${
+          darkMode ? 'bg-[#525252]' : 'bg-[#eaeaea]'
+        } ${
+          darkMode ? 'shadow-[#6f6f6f]' : 'shadow-[#cfcfcf]'
+        } max-[450px]:text-[0.8em]`}
+      >
+        Privacy Policy
       </button>
     </div>
   );
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -15,54 +15,56 @@ function Settings() {
   const { loggedIn, user } = useContext(AuthContext);
 
   return (
-    <div className="h-full min-h-[60vh] mx-[2%] mt-[10%] flex flex-col">
-      {loggedIn ? (
-        <div className="mx-auto">
-          <div className="mb-[30px] flex items-center">
-            <UserImage />
-            <h2 className="ml-[15px]">
-              {user ? (
-                `${user.firstName} ${user.lastName}`
-              ) : (
-                <CircularProgress />
-              )}
-            </h2>
-          </div>
-          <div className="mb-[30px] flex items-center">
-            <EmailRoundedIcon
-              sx={{
-                width: '36px',
-                height: '36px',
-                marginLeft: '15px',
-                marginRight: '15px',
-                color: darkMode ? 'white' : '#678B85',
-              }}
-            />
-            <p className="ml-[15px]">
-              {' '}
-              {user ? `${user.email}` : <CircularProgress />}
-            </p>
-          </div>
-          <div className="w-[100%] flex flex-col justify-center items-center">
-            <div className="mb-[15px] w-[100%]">
-              <ChangePasswordButton />
+    <div className="h-full min-h-[60vh] mx-[2%] mt-[10%]">
+      <div className="flex flex-col">
+        {loggedIn ? (
+          <div className="mx-auto">
+            <div className="mb-[30px] flex items-center">
+              <UserImage />
+              <h2 className="ml-[15px]">
+                {user ? (
+                  `${user.firstName} ${user.lastName}`
+                ) : (
+                  <CircularProgress />
+                )}
+              </h2>
             </div>
-            <DeleteAccountButton />
+            <div className="mb-[30px] flex items-center">
+              <EmailRoundedIcon
+                sx={{
+                  width: '36px',
+                  height: '36px',
+                  marginLeft: '15px',
+                  marginRight: '15px',
+                  color: darkMode ? 'white' : '#678B85',
+                }}
+              />
+              <p className="ml-[15px]">
+                {' '}
+                {user ? `${user.email}` : <CircularProgress />}
+              </p>
+            </div>
+            <div className="w-[100%] flex flex-col justify-center items-center">
+              <div className="mb-[15px] w-[100%]">
+                <ChangePasswordButton />
+              </div>
+              <DeleteAccountButton />
+            </div>
           </div>
+        ) : (
+          <p className="text-center mt-[50px] font-bold text-xl">
+            Please log in to view this page
+          </p>
+        )}
+        {!loggedIn && width < 500 && (
+          <div className="flex justify-center items-center gap-[15px] my-[30px]">
+            <LogInButton />
+            <SignUpButton />
+          </div>
+        )}
+        <div className="my-[30px] mx-auto">
+          <Footer />
         </div>
-      ) : (
-        <p className="text-center mt-[50px] font-bold text-xl">
-          Please log in to view this page
-        </p>
-      )}
-      {!loggedIn && width < 500 && (
-        <div className="flex justify-center items-center gap-[15px] my-[30px]">
-          <LogInButton />
-          <SignUpButton />
-        </div>
-      )}
-      <div className="my-[30px]">
-        <Footer />
       </div>
     </div>
   );
