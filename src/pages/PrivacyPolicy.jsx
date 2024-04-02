c9c6d2ec04e9c7e4e920e00b9c30f4fb64bfe00b
anapatriciagomes <anapatrigomes@gmail.com>
2024-04-02 15:35:12 +0100

    privacy policy style update

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -5,8 +5,8 @@ import SecurityRoundedIcon from '@mui/icons-material/SecurityRounded';
 function PrivacyPolicy() {
   const { darkMode } = useContext(ThemeContext);
   return (
-    <div className='m-[2%] mt-[30px]'>
-      <div className='flex items-center gap-[10px]'>
+    <div className="m-[2%] my-[30px] pb-[30px]">
+      <div className="flex items-center gap-[10px] mt-[30px] mb-[10px] h-[70px]">
         <SecurityRoundedIcon
           sx={{
             color: darkMode ? 'white' : '#678B85',
@@ -22,7 +22,7 @@ function PrivacyPolicy() {
           Privacy Policy
         </h2>
       </div>
-      <p className='mt-[40px]'>
+      <p className="mt-[40px]">
         Welcome to AppliSnap! This <strong>Privacy Policy</strong> explains how
         we collect, use, and protect information when you use our application.
       </p>
@@ -35,7 +35,7 @@ function PrivacyPolicy() {
         Information We Collect
       </h2>
       <p>We collect the following information:</p>
-      <ul className='mt-[20px] list-disc ml-10'>
+      <ul className="mt-[20px] list-disc ml-10">
         <li>Unique user ID associated with your profile</li>
         <li>User email</li>
         <li>First and last name</li>
@@ -52,26 +52,26 @@ function PrivacyPolicy() {
       >
         Purposes of Collection
       </h2>
-      <p className='mt-4'>
+      <p className="mt-4">
         <strong>User ID:</strong>
       </p>
-      <ul className='mt-[20px] list-disc ml-10'>
+      <ul className="mt-[20px] list-disc ml-10">
         <li>Enable the core functionality of the App and its expansion</li>
         <li>Personalize and improve your experience</li>
         <li>Troubleshoot technical issues</li>
       </ul>
 
-      <p className='mt-4'>
+      <p className="mt-4">
         <strong>User Email, First and Last Name:</strong>
       </p>
-      <ul className='mt-[20px] list-disc ml-10'>
+      <ul className="mt-[20px] list-disc ml-10">
         <li>Customization of user experience</li>
       </ul>
 
-      <p className='mt-4'>
+      <p className="mt-4">
         <strong>Job Applications:</strong>
       </p>
-      <ul className='mt-[20px] list-disc ml-10'>
+      <ul className="mt-[20px] list-disc ml-10">
         <li>Solely for displaying them in the platform for the user</li>
       </ul>
 
@@ -83,7 +83,7 @@ function PrivacyPolicy() {
         How We Use Your Information
       </h2>
       <p>We use the collected information to:</p>
-      <ul className='mt-[20px] list-disc ml-10'>
+      <ul className="mt-[20px] list-disc ml-10">
         <li>Customize your experience</li>
         <li>Improve our services and features</li>
         <li>Conduct analytics for performance enhancements</li>
@@ -98,7 +98,7 @@ function PrivacyPolicy() {
         Data Security
       </h2>
       <p>We take the security of your information seriously:</p>
-      <ul className='mt-[20px] list-disc ml-10'>
+      <ul className="mt-[20px] list-disc ml-10">
         <li>
           Your user ID, email, first and last name, and job applications are
           securely stored and transmitted.
@@ -120,12 +120,12 @@ function PrivacyPolicy() {
         We do not disclose your user ID, email, first and last name, or job
         applications to third parties except:
       </p>
-      <ul className='mt-[20px] list-disc ml-10'>
+      <ul className="mt-[20px] list-disc ml-10">
         <li>To comply with legal obligations</li>
         <li>To protect and defend our rights and property</li>
       </ul>
 
-      <p className='mt-3'>
+      <p className="mt-3">
         We do not sell your information for marketing purposes.
       </p>
 
@@ -164,10 +164,12 @@ function PrivacyPolicy() {
       <p>
         If you have questions about this Privacy Policy, please{' '}
         <a
+          href="mailto:applisnap2024@gmail.com"
+          target="_blank"
+          rel="noreferrer"
           className={`font-bold ${
             darkMode ? 'text-[#ff8956]' : 'text-[#f06c35]'
           }`}
-          href='mailto:applisnap2024@gmail.com'
         >
           contact us.
         </a>
